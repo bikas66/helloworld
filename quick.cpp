ff58f998dc2f2d8c551939e1dd90a38f42dd9da4
bikas66 <bikas66@gmail.com>
2024-05-13 15:33:18 +0530

    aad
    adad

---
+++ quick.cpp
@@ -0,0 +1,82 @@
+
+//Quick sort 
+
+#include<stdio.h>
+void quicksort(int*,int,int);
+int partition(int*,int,int);
+
+void swap(int*,int*);
+int main()
+{
+    int arr[20],n,l,p,check,m;
+    printf("Enter the no. of elements:");
+    scanf("%d",&n);
+    printf("\n Enter the elements of the array:");
+    for(l=0;l<n;l++)
+    {
+      scanf("%d",&arr[l]);
+    }
+      quicksort(arr,0,n-1);
+
+      printf("\n Elements of the arrray after sorting: ");
+      for(l=0;l<n;l++)
+      {
+        printf("%d\t",arr[l]);
+      }
+    return 0;
+}
+void quicksort(int arr[],int s,int e)
+
+{
+    int l,mid,i;
+    if(s>=e)
+      return;
+    else
+    {
+        printf("\n PIVOT ELEMENT:%d\n",arr[e]);
+       
+        int p=partition(arr,s,e);
+
+        printf("\n pass:");
+        for(i=s;i<=e;i++)
+          printf("%d ",arr[i]);
+        printf("\n pass:");
+        for(i=s;i<=p-1;i++)
+
+          printf("%d ",arr[i]);
+        quicksort(arr,s,p-1);
+        printf("\n pass:");
+        for(i=p+1;i<=e;i++)
+
+          printf("%d ",arr[i]);
+        quicksort(arr,p+1,e);
+       /* printf("\n pass:");
+        for(i=s;i<=e;i++)
+          printf("%d ",arr[i]);*/
+    }
+}
+int partition(int arr[],int s,int e)
+
+{
+    int pivot=arr[e];
+    int i=s-1;
+    int j;
+   
+      for(j=s;j<e;j++)
+    {
+        if(arr[j]<=pivot)
+        {
+            i++;
+            swap(&arr[i],&arr[j]);
+        }
+    }
+    swap(&arr[i+1],&arr[e]);
+    return(i+1);
+}
+void swap(int *v1,int *v2)
+{
+    int temp=*v1;
+    *v1=*v2;
+    *v2=temp;
+}
+
